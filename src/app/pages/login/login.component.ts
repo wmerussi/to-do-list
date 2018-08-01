7f397cb8a694728ba9b16acfc8ba2292dbf541f8
William Teixeira Merussi <wmerussi@gmail.com>
2018-08-01 16:39:43 -0300

    added message component to login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -4,6 +4,8 @@ import { Router } from '@angular/router'
 import { AuthService } from '../../services/auth.service'
 import { UniqueNumberService } from '../../services/unique-number.service'
 
+import { Message } from '../../../ui/models/message.model'
+
 const firebaseError = [
   'auth/wrong-password',
   'auth/invalid-email',
@@ -23,6 +25,8 @@ export class LoginComponent implements OnInit {
   public emailFieldId: string
   public passwordFieldId: string
 
+  public message: Message
+
   constructor(
     private auth: AuthService,
     private router: Router,
@@ -40,25 +44,38 @@ export class LoginComponent implements OnInit {
   }
 
   public emailSignIn() {
+    this.message = null
+
     this.auth.signIn(this.email, this.password).then(
       () => { }, // app.component will listen and redirect to home page
       (error) => {
         /** Check if e-mail is registered or it has a valid e-mail/password */
         if (firebaseError.includes(error.code)) {
-          console.log('invalid e-mail or password')
+          this.message = new Message({
+            text: 'E-mail e/ou senha incorreto(s).',
+            type: 'danger',
+          })
           return
         }
 
         /** Informs an unknown error */
-        console.log('unknown error')
+        this.message = new Message({
+          text: 'Erro desconhecido, favor entrar em contato com o suporte.',
+          type: 'danger',
+        })
       },
     )
   }
 
   public socialSignIn(type: string) {
+    this.message = null
+
     this.auth.socialSignIn(type).then(
-      success => console.log('social success', success),
-      error => console.log('social error', error),
+      () => { }, // app.component will listen and redirect to home page
+      () => this.message = new Message({
+        text: 'Erro ao tentar entrar via rede social.',
+        type: 'danger',
+      }),
     )
   }
 }
