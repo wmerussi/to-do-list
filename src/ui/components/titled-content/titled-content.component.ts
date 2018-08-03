b9c630dfcf051860a6e2c5f4b72dabf2f1fd2637
William Teixeira Merussi <wmerussi@gmail.com>
2018-08-03 09:17:24 -0300

    added color type and BEM class style

--- src/ui/components/titled-content/titled-content.component.ts
+++ src/ui/components/titled-content/titled-content.component.ts
@@ -4,9 +4,16 @@ import { Component } from '@angular/core'
   selector: 'ui-titled-content',
   templateUrl: './titled-content.component.html',
   styleUrls: ['./titled-content.component.scss'],
-  inputs: ['title'],
+  inputs: ['color', 'title'],
 })
 export class TitledContentComponent {
   /** Inputs */
+  public color: string = 'light'
   public title: string
+
+  public getTitleClasses(): Object {
+    return {
+      [`titled-content__title--${ this.color }`]: !!this.color,
+    }
+  }
 }
