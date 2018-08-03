c330906ae35f822a8204f194cc419098da81ad57
William Teixeira Merussi <wmerussi@gmail.com>
2018-08-03 12:55:28 -0300

    Changed login method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,23 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { Router } from '@angular/router'
 
-import { AuthService } from './services/auth.service'
+import { AngularFireAuth } from 'angularfire2/auth'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [AuthService],
 })
 export class AppComponent implements OnInit {
-  constructor(private auth: AuthService, private cd: ChangeDetectorRef, private route: Router) { }
+  constructor(private angularFire: AngularFireAuth, private route: Router) { }
 
   ngOnInit() {
     /** Listen to Auth State and go to the proper page when logged in or out */
-    this.auth.authState().subscribe((userIsLoggedIn) => {
-      if (userIsLoggedIn) {
-        this.route.navigateByUrl('/').then(() => this.cd.detectChanges())
-        return
-      }
+    this.angularFire.auth.onAuthStateChanged(() => {
+      const user = this.angularFire.auth.currentUser
+      const rootPage = !!user ? '/' : '/login'
 
-      this.route.navigateByUrl('/login').then(() => this.cd.detectChanges())
+      this.route.navigateByUrl(rootPage)
     })
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,23 +3,9 @@ import { Injectable } from '@angular/core'
 import { AngularFireAuth } from 'angularfire2/auth'
 import * as firebase from 'firebase'
 
-import { BehaviorSubject, Observable } from 'rxjs'
-
 @Injectable()
 export class AuthService {
-  private authStateBs: BehaviorSubject<boolean> = new BehaviorSubject(false)
-
-  constructor(private angularFireAuth: AngularFireAuth) {
-    this.checkUserState()
-  }
-
-  /**
-   * Method to listen if the user is logged in
-   * @returns { Observable<boolean> }
-   */
-  public authState(): Observable<boolean> {
-    return this.authStateBs.asObservable()
-  }
+  constructor(private angularFireAuth: AngularFireAuth) { }
 
   /**
    * Register a new user
@@ -48,9 +34,7 @@ export class AuthService {
    */
   public signIn(email: string, password: string): Promise<firebase.auth.UserCredential> {
     /** Stores the login on Firebase Auth Session */
-    return this.angularFireAuth.auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL).then(
-      () => this.angularFireAuth.auth.signInWithEmailAndPassword(email, password),
-    )
+    return this.angularFireAuth.auth.signInWithEmailAndPassword(email, password)
   }
 
   /**
@@ -83,18 +67,5 @@ export class AuthService {
     }
 
     return this.angularFireAuth.auth.signInWithRedirect(provider)
-  }
-
-  /**
-   * Get user's data from social network
-   * @returns { Promise<firebase.auth.UserCredential> }
-   */
-  public getSocialData(): Promise<firebase.auth.UserCredential> {
-    return this.angularFireAuth.auth.getRedirectResult()
-  }
-
-  /** Subscribes to firebase user state and inform 'authStateBs' if it's logged in */
-  private checkUserState() {
-    this.angularFireAuth.auth.onAuthStateChanged(user => this.authStateBs.next(!!user))
   }
 }
